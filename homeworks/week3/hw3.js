99773fe43f9d078c73ad9133653858e7fce5fd93
Chen Murray <murray501029@Chende-MacBook-Pro.local>
2019-05-08 13:01:53 +0800

    week3 finish

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,25 @@
 function isPrime(n) {
-  console.log(n);
+  if (n === 1) {
+    return false;
+  }
+
+  let outcome = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      outcome = false;
+    }
+  }
+  return outcome;
 }
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(10));
+console.log(isPrime(37));
+console.log(isPrime(38));
+console.log(isPrime(40));
+console.log(isPrime(99991));
+
 
 module.exports = isPrime;
