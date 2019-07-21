1502f10b9dba454c46eeb081652896227fade1b4
Irene <itinerant.irene@gmail.com>
2019-07-21 16:02:50 +0800

    done of hw2

--- homeworks/week13/hw2/index.js
+++ homeworks/week13/hw2/index.js
@@ -1,23 +1,24 @@
 /* eslint-env jquery */
 
-// 問題：後來新增的元素無法擁有前面已經用 for 迴圈來設定好的監聽事件 `addEventListener`
-
-// 方法一：原版第七週改為正常版！使用事件代理 e.target
-$('.list-group').click(
-  (eventData) => {
-    // console.log(eventData.target.classList);
-    if (eventData.target.classList.contains('list-group-item')) {
-      alert('success');
-    }
+$('.btn-primary').click(
+  () => {
+    // console.log($('input[type=text]').val());
+    const item = "<li class='list-group-item'> <input class='check' type='checkbox'> <span> {{item}} </span> <button class='btn btn-outline-primary'>X</button> </li>";
+    const newItem = item.replace('{{item}}', $('input[type=text]').val());
+    $('.list-group').append(newItem);
   },
 );
 
-// 方法二：使用 this => I guess 我猜的
-// 之後再學 arrow function 的 this 跟一般 function 到底差在哪裡？
-// $('.list-group-item').click(() => {
-//   $(this).toggleClass('done');
-// });
+$('.list-group').click(
+  (eventData) => {
+    // console.log(eventData.target);
 
-// $('.list-group-item').click(function () {
-//   $(this).toggleClass('done');
-// });
+    if (eventData.target.classList.contains('check')) {
+      $(eventData.target).closest('.list-group-item').toggleClass('done');
+    }
+
+    if (eventData.target.classList.contains('btn-outline-primary')) {
+      $(eventData.target).closest('.list-group-item').remove();
+    }
+  },
+);
